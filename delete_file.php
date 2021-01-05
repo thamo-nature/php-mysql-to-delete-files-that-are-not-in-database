58dd0c7804454efe58cc9cdf77b728bb5949a6a3
Thamothiran Naturalist <29664661+thamo-nature@users.noreply.github.com>
2021-01-05 13:41:09 +0530

    Update delete_file.php

--- delete_file.php
+++ delete_file.php
@@ -5,21 +5,21 @@ $query = $database->connection->query("SELECT image,pdf FROM posts where usernam
 
 $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
-//first column
- $image_0 = $result[0]['image']; //first row 
- // $image_1 = $result[1]['image']; //second row
- // $image_2 = $result[2]['image']; //third row
+   //first column
+$image_0 = $result[0]['image']; //first row 
+   // $image_1 = $result[1]['image']; //second row
+   // $image_2 = $result[2]['image']; //third row
  
 
-//second column
+   //second column
  $pdf1_0 = $result[0]['pdf']; 
- //$pdf1_1 = $result[1]['pdf']; //second row
- //$pdf1_2 = $result[2]['pdf']; //third row
+   //$pdf1_1 = $result[1]['pdf']; //second row
+   //$pdf1_2 = $result[2]['pdf']; //third row
 
 //third column
- //$col2_0 = $min_price_value[0]['image2'];
- //$col2_1 = $min_price_value[1]['image2']; //second row
- //$col2_2 = $min_price_value[2]['image2']; //third row
+ //$col2_0 = $result[0]['file3'];
+ //$col2_1 = $result[1]['file3']; //second row
+ //$col2_2 = $result[2]['file3']; //third row
 
 //files not to be deleted,query values
 $leave_files = array($image_0,$pdf1_0); 

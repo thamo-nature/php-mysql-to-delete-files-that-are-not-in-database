e8cc0d32bcd2624f64b918382b19cd23fd70cc00
Thamothiran Naturalist <29664661+thamo-nature@users.noreply.github.com>
2021-01-05 13:33:41 +0530

    Create delete_file.php

---
+++ delete_file.php
@@ -0,0 +1,34 @@
+<?php
+// add connection variable db file
+
+$query = $database->connection->query("SELECT image,pdf FROM posts where username='$database->logged_user' ");
+
+$result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+//first column
+ $image_0 = $result[0]['image']; //first row 
+ // $image_1 = $result[1]['image']; //second row
+ // $image_2 = $result[2]['image']; //third row
+ 
+
+//second column
+ $pdf1_0 = $result[0]['pdf']; 
+ //$pdf1_1 = $result[1]['pdf']; //second row
+ //$pdf1_2 = $result[2]['pdf']; //third row
+
+//third column
+ //$col2_0 = $min_price_value[0]['image2'];
+ //$col2_1 = $min_price_value[1]['image2']; //second row
+ //$col2_2 = $min_price_value[2]['image2']; //third row
+
+//files not to be deleted,query values
+$leave_files = array($image_0,$pdf1_0); 
+
+foreach( glob("uploads/path/*") as $file ) {
+  if( !in_array(basename($file), $leave_files) ){
+    unlink($file); //delete the file.
+  //echo $file."</br>";
+  }
+}
+
+?>
